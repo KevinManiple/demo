92bfc65317d2d63cfbefa8a7327722ccc0ea6d46
Kai.Zhao <zakaz168@gmail.com>
2016-02-03 14:52:57 +0800

    ignore

--- src/main/java/thread/ThreadDemo.java
+++ src/main/java/thread/ThreadDemo.java
@@ -2,70 +2,55 @@ package thread;
 
 public class ThreadDemo {
 
-	public static void main(String[] args) throws InterruptedException {
-		Object o1 = new Object();
-		Object o2 = new Object();
-		Object o3 = new Object();
-		Thread t1 = new Thread(new MyThread(o1, o2));
-		Thread t2 = new Thread(new MyThread(o2, o3));
-		Thread t3 = new Thread(new MyThread(o3, o1));
-		t1.start();
-		Thread.sleep(500);
-		t2.start();
-		Thread.sleep(500);
-		t3.start();
-	}
+    public static void main(String[] args) throws InterruptedException {
+        Object o1 = new Object();
+        Object o2 = new Object();
+        Object o3 = new Object();
+        Thread t1 = new Thread(new MyThread(o1, o2));
+        Thread t2 = new Thread(new MyThread(o2, o3));
+        Thread t3 = new Thread(new MyThread(o3, o1));
+        t1.start();
+        Thread.sleep(500);
+        t2.start();
+        Thread.sleep(500);
+        t3.start();
+    }
 }
 
 class MyThread extends Thread {
 
-	private Object	obj1;
-	private Object	obj2;
+    private Object obj1;
+    private Object obj2;
 
-	public MyThread(Object obj1, Object obj2) {
-		this.obj1 = obj1;
-		this.obj2 = obj2;
-	}
+    public MyThread(Object obj1, Object obj2) {
+        this.obj1 = obj1;
+        this.obj2 = obj2;
+    }
 
-	@Override
-	public void run() {
-		String name = Thread.currentThread().getName();
-		System.out.println(name + " acquiring lock on " + obj1);
-		synchronized (obj1) {
-			System.out.println(name + " acquired lock on " + obj1);
-			work();
-			System.out.println(name + " acquiring lock on " + obj2);
-			synchronized (obj2) {
-				System.out.println(name + " acquired lock on " + obj2);
-				work();
-			}
-			System.out.println(name + " released lock on " + obj2);
-		}
-		System.out.println(name + " released lock on " + obj1);
-		System.out.println(name + " finished execution.");
-	}
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " acquiring lock on " + obj1);
+        synchronized (obj1) {
+            System.out.println(name + " acquired lock on " + obj1);
+            work();
+            System.out.println(name + " acquiring lock on " + obj2);
+            synchronized (obj2) {
+                System.out.println(name + " acquired lock on " + obj2);
+                work();
+            }
+            System.out.println(name + " released lock on " + obj2);
+        }
+        System.out.println(name + " released lock on " + obj1);
+        System.out.println(name + " finished execution.");
+    }
 
-	private void work() {
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    private void work() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public Object getObj1() {
-		return obj1;
-	}
-
-	public void setObj1(Object obj1) {
-		this.obj1 = obj1;
-	}
-
-	public Object getObj2() {
-		return obj2;
-	}
-
-	public void setObj2(Object obj2) {
-		this.obj2 = obj2;
-	}
 }
