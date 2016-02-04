ff06e9b3ca4a4cd780ebd08db3f34c1203cfbb46
Kai.Zhao <zakaz168@gmail.com>
2016-02-04 15:47:37 +0800

    test

--- src/main/java/proxy/ProxyExample.java
+++ src/main/java/proxy/ProxyExample.java
@@ -1,9 +1,5 @@
 package proxy;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-
 public class ProxyExample {
 
    /* public static void main(String[] args) {
